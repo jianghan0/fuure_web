05fb9516faaacc70fa3381378a817e1a336715cc
千田 俊 <jh.wk.dev@gmail.com>
2019-09-27 22:35:23 +0900

    fix plugin

--- plugins/shopify-client.js
+++ plugins/shopify-client.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import fetch from 'node-fetch'
 import Client from 'shopify-buy'
+import fetch from 'node-fetch'
 
-const shopify = Client.buildClient({
-  domain: process.env.NUXT_ENV_STORE_URL,
-  storefrontAccessToken: process.env.NUXT_ENV_STORE_FRONT_ACCESS_TOKEN
-}, fetch);
+const shopify = Client.buildClient(
+  {
+    domain: process.env.NUXT_ENV_STORE_URL,
+    storefrontAccessToken: process.env.NUXT_ENV_STORE_FRONT_ACCESS_TOKEN,
+  },
+  fetch
+)
 
-Vue.prototype.$shopify = shopify
+export default ({ _app }, inject) => {
+  inject('shopify', shopify)
+}
